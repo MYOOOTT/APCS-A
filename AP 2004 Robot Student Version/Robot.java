2899896e6347a8c2075a1bc5cdc801c64d6568f1
MYOOOTT <mnguy1231@gmail.com>
2017-01-10 12:41:09 -0800

    Finished the Robot AP short answer!

--- AP 2004 Robot Student Version/Robot.java
+++ AP 2004 Robot Student Version/Robot.java
@@ -4,53 +4,59 @@
  */
 
 public class Robot {
-    public int[] hall;
-    public int pos; // current position in the hall AND IS USUALLY PRIVATE
+    private int[] hall;
+    private int pos; // current position in the hall AND IS USUALLY PRIVATE
     private boolean facingRight; // true means is facing right
 
     // constructor is shown
     public Robot() {
         hall = new int[4];
-        hall[0] = 0;
-        hall[1] = 0;
-        hall[2] = 0;
-        hall[3] = 0;
+        hall[0] = 1;
+        hall[1] = 1;
+        hall[2] = 2;
+        hall[3] = 2;
         pos = 1;
         facingRight = true;
     } 
 
     // returns true if Robot has a wall in front of it 
     private boolean forwardMoveBlocked() {
-        if (pos  >= hall.length  || pos <= 0) {
+        if (pos == hall.length - 1 && facingRight == true) {
+            return true;
+        } 
+        if (pos == 0 && facingRight == false) {
             return true;
         } else {
             return false;
+
         }
-    } 
+    }
 
     // make one move and update the status of the Robot
     // also updates the status of the hall
-    public void move() {
-        int numItems = 0;  
+    private void move() {
+        int numItems = 0;
         int moveAmt = 0;
-        if (facingRight = true) {
-            moveAmt = 1;
-        } else {
-            moveAmt = -1;
-        }
-        if (hall[pos] == 1 && forwardMoveBlocked() == false) {
-            hall[pos] = numItems;
+        
+        // picks up an item if there is more an item on the floor
+        if (hall[pos] >= 1) {
+            numItems = hall[pos];
             hall[pos] = numItems - 1;
-            pos += moveAmt;
-        } else if (hall[pos] >= 1) {
-            hall[pos] = numItems;
-            hall[pos] = numItems - 1;
-        } else if (hall[pos] == 0) {
-            if (forwardMoveBlocked() == false) {
-                pos += moveAmt;
+            System.out.println("Picked up an item.");
+        } 
+        //checks if there are no items first, then if it can move forward or not. If it hits a wall, it turns around.
+        if (hall[pos] == 0) {
+            if (forwardMoveBlocked() == false && facingRight == true) {
+                pos += 1;
+                System.out.println("Moved forward.");
+            } else if (forwardMoveBlocked() == false && facingRight == false) {
+                pos -= 1;
+                System.out.println("Moved backwards.");
             } else {
-                facingRight = false;
+                facingRight = !facingRight;
+                System.out.println("Turned around.");
             }
+
         }
 
     }  
@@ -62,17 +68,18 @@ public class Robot {
     public int clearHall() {
         int moves = 0;
         while (hallIsClear() != true) {
+            moves++;
+            System.out.println();
+            System.out.println("Move " + moves);
+            System.out.println("------");
             move();
-            if (forwardMoveBlocked() == true) {
-                
-            }
         }
-            return 0;
+        return moves;
     } 
 
     // returns "true" if the hall contains no items
     // otherwise, this method will return "false"
-    public boolean hallIsClear() {
+    private boolean hallIsClear() {
         /* to be implemented in part (c) */
         // false = there are items still there
         int tileClear = 0;
--- AP 2004 Robot Student Version/RobotDriver.java
+++ AP 2004 Robot Student Version/RobotDriver.java
@@ -2,8 +2,8 @@ public class RobotDriver
 {
     public static void main(String[] args) {
         Robot c3po = new Robot();
-        //int movesTaken = c3po.clearHall();
-        //System.out.println("The robot cleared the hall in " + movesTaken + " moves"); 
+        int movesTaken = c3po.clearHall();
+        System.out.println("The robot cleared the hall in " + movesTaken + " moves"); 
         
     }    
 }
